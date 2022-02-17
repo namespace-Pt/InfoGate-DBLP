1929c55a4f59839dbe1a5d084a747e3406d0d785
namespace-Pt <namespace.pt@gmail.com>
2022-02-17 04:14:54 +0000

    1

--- docs/Experiments.md
+++ docs/Experiments.md
@@ -2,10 +2,10 @@
 
 |Model|Weighter|sequence length|k|batch size|learning rate|ACC|MRR|nDCG@10|AUC|
 |:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|
-|GraphSage|:x:|32|:x:|800|1e-5|||||
-|GateSage|CNN|32|8|800|1e-5|||||
+|GraphSage|:x:|32|:x:|800|1e-5|0.702|0.937|0.8411|0.9920|
+|GateSage|CNN|32|8|800|1e-5|0.6689|0.7684|0.8212|0.9903|
 |GateSage|TFM|32|8|800|1e-5|||||
-|GateSage|First|32|8|800|1e-5|||||
+|GateSage|First|32|8|800|1e-5|0.6342|0.7411|0.8008|0.9889|
 |GateSage|BM25|32|8|800|1e-5|||||
 |GateSage|Random|32|8|800|1e-5|||||
 |GateSage|KeyBert|32|8|800|1e-5|||||
--- src/main/keywords.py
+++ src/main/keywords.py
@@ -1,6 +1,9 @@
 import os
 import argparse
 import subprocess
+import warnings
+warnings.filterwarnings("ignore")
+
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
@@ -38,7 +41,7 @@ if args.enable_gate == "bm25":
 
                 query_and_neighbors = [bm25(x) for x in query_and_neighbors]
                 key_and_neighbors = [bm25(x) for x in key_and_neighbors]
-                g.write("|\'|".join(query_and_neighbors) + "\t" + "|\'|".join(key_and_neighbors))
+                g.write("|\'|".join(query_and_neighbors) + "\t" + "|\'|".join(key_and_neighbors) + "\n")
             g.close()
 
 
@@ -53,24 +56,21 @@ elif args.enable_gate == "keybert":
 
         texts = []
         with open(text_path) as f:
-            new_text_path = os.path.join(os.path.split(text_path)[0], "bm25.tsv")
+            new_text_path = os.path.join(os.path.split(text_path)[0], "keybert.tsv")
             g = open(new_text_path, "w")
             for line in tqdm(f, total=int(subprocess.check_output(["wc", "-l", text_path]).decode("utf-8").split()[0])):
                 query_and_neighbors, key_and_neighbors = line.strip('\n').split('\t')[:2]
-                query_and_neighbors = query_and_neighbors.split('|\'|')
-                key_and_neighbors = key_and_neighbors.split('|\'|')
+                query_and_neighbors = query_and_neighbors.split('|\'|')[:5]
+                key_and_neighbors = key_and_neighbors.split('|\'|')[:5]
 
-                query_and_neighbors_keywords = []
-                for x in query_and_neighbors:
-                    text = kw_model.extract_keywords(x, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=100)
-                    text = " ".join([kwd[0] for kwd in text])
-                    query_and_neighbors.append(text)
+                query_and_neighbors_keywords = kw_model.extract_keywords(query_and_neighbors, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=10)
+                for i,x in enumerate(query_and_neighbors_keywords):
+                    query_and_neighbors_keywords[i] = " ".join([kwd[0] for kwd in x])
 
-                key_and_neighbors_keywords = []
-                for x in key_and_neighbors:
-                    text = kw_model.extract_keywords(x, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=100)
-                    text = " ".join([kwd[0] for kwd in text])
-                    key_and_neighbors.append(text)
-                g.write("|\'|".join(query_and_neighbors) + "\t" + "|\'|".join(key_and_neighbors))
+                key_and_neighbors_keywords = kw_model.extract_keywords(key_and_neighbors, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=10)
+                for i,x in enumerate(key_and_neighbors_keywords):
+                    key_and_neighbors_keywords[i] = " ".join([kwd[0] for kwd in x])
+
+                g.write("|\'|".join(query_and_neighbors_keywords) + "\t" + "|\'|".join(key_and_neighbors_keywords) + "\n")
             g.close()
 
